a8064398f8e132c74cf672024fe9877c942256e7
MattColaProgrammer93 <mattchoque115@gmail.com>
2022-07-13 14:07:11 -0700

    Created the student and advisor classes.

---
+++ NestedObjects/Student.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NestedObjects
+{
+    /// <summary>
+    /// Represents a individual college student
+    /// </summary>
+    public class Student
+    {
+        /// <summary>
+        /// The legeal first name
+        /// </summary>
+        public string FirstName { get; set; }
+
+        /// <summary>
+        /// The legeal last name
+        /// </summary>
+        public string LastName { get; set; }
+
+        /// <summary>
+        /// The assigned school email adress
+        /// </summary>
+        public string SchoolEmail { get; set; }
+
+        /// <summary>
+        /// The student's phone number
+        /// </summary>
+        public string PhoneNumber { get; set; }
+        /// <summary>
+        /// The student's advisor
+        /// </summary>
+        public Advisor SignedAdvisor { get; set; }
+    }
+}
